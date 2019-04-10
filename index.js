73f368c1e7708119a809f133f37d3b0b897c8fcf
Alan Houston <alanhouston5000@gmail.com>
2019-04-10 13:25:51 -0500

    minors

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ const GripSwitchRoutes = require("./routes/GripswitchRoutes");
 const MaxDayRoutes = require("./routes/MaxDayRoutes");
 const PushUpRoutes = require("./routes/PushUpRoutes");
 
-//to allow fetch calls to localhost:3000
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+//to allow fetch calls to localhost:3000 working locally
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.use(bodyParser.json());
 app.use(MaxEffortRoutes);
